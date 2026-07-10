caeb730d199f8f148ed4d5e3271984311f85d969
iermiloff <109422436+iermiloff@users.noreply.github.com>
2026-07-11 00:40:10 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 echo "Ожидаю запуск базы данных и применяю миграции Alembic..."
 alembic upgrade head
 echo "Миграции успешно применены! Запускаю Telegram-бота..."
-exec python -m main
+exec python -m bot.main
