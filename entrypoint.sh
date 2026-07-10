534fb8b30eccc1f9b85946fc522a3965c29851f9
iermiloff <109422436+iermiloff@users.noreply.github.com>
2026-07-11 00:49:14 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-echo "Ожидаю запуск базы данных и применяю миграции Alembic..."
+
+echo "⏳ Ожидаю запуск базы данных PostgreSQL (${DB_HOST}:${DB_PORT})..."
+
+# Цикл, который проверяет доступность порта каждые 1 секунду
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
+  sleep 1
+done
+
+echo "✅ База данных успешно запущена! Применяю миграции Alembic..."
 alembic upgrade head
-echo "Миграции успешно применены! Запускаю Telegram-бота..."
+
+echo "✨ Миграции успешно применены! Запускаю Telegram-бота..."
 exec python -m bot.main
